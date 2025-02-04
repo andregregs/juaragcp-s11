43f46c36d359edc613817b3fce7f831e90162af4
Andre Gregori Sangari <andresangari12@gmail.com>
2025-02-04 14:06:24 +0700

    edit script

--- lab-solutions/Secure Software Delivery Challenge Lab/script.sh
+++ lab-solutions/Secure Software Delivery Challenge Lab/script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "Starting Execution"
-
 export PROJECT_ID=$(gcloud config get-value project)
 export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID \
 --format='value(projectNumber)')
